a8a69d54d0f06b24b4646bd649c0fe785381cd99
g21turbo <dg21stew@gmail.com>
2023-10-27 23:38:37 -0700

    added prime counter and for loop that checks for three primes

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("0 is " + (isPrime(0) ? "" : "NOT ") + "a prime number");
-        System.out.println("1 is " + (isPrime(1) ? "" : "NOT ") + "a prime number");
-        System.out.println("2 is " + (isPrime(2) ? "" : "NOT ") + "a prime number");
-        System.out.println("3 is " + (isPrime(0) ? "" : "NOT ") + "a prime number");
+        int primeCounter = 0;
 
-        System.out.println("8 is " + (isPrime(8) ? "" : "NOT ") + "a prime number");
-        System.out.println("17 is " + (isPrime(17) ? "" : "NOT ") + "a prime number");
-
+        for (int i = 10; i <= 50; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+                primeCounter++;
+                if (primeCounter == 3) {
+                    System.out.println("Found 3 - Exiting th loop");
+                    break;
+                }
+            }
+        }
     }
-
 
     public static boolean isPrime(int wholeNumber) {
 
@@ -24,8 +27,6 @@ public class Main {
                 return  false;
             }
         }
-
         return true;
     }
-
 }
